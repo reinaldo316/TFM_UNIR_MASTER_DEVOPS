74218d7f816a11f7690d41bb5b16b9992f17a87f
Edgar guerra <reinaldo316@gmail.com>
2023-07-22 16:07:01 -0500

    terraform validate y plan actions

--- networks.tf
+++ networks.tf
@@ -1,18 +1,9 @@
-resource "aws_vpc" "vpc_suse" {
-  cidr_block = "10.1.1.0/16"
-}
 
-resource "aws_subnet" "subnet_suse" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.1.1.0/24"
-   availability_zone = "us-east-1a" 
-}
-
-resource "aws_vpc" "vpc_ws" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "subnet_ws" {
+resource "aws_subnet" "subnet" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.0.0/24"
    availability_zone = "us-east-1a" 
